6642af97c9fd3b6381272daf128d4ebad7d0f239
Robert-Kusz <kooshservers@googlemail.com>
2022-07-27 16:15:55 +0100

    changed pom file to reflect sql and updated schema file to create tables for item and order

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,23 +1,22 @@
-drop schema if exists ims;
+drop schema IF EXISTS ims;
 CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(40) DEFAULT NULL,
-  `surname` VARCHAR(40) DEFAULT NULL,
-  PRIMARY KEY (`id` INT (5))
+  `id` INT(11)  AUTO_INCREMENT,
+  `first_name` VARCHAR(40) ,
+  `surname` VARCHAR(40) ,
+  PRIMARY KEY (`id` )
 );
-CREATE TABLE IF NOT EXISTS `ims` . `items` (
-  `Item_id INT(5) NOT NULL AUTO_INCREMENT,
-  `item_name` VARCHAR(40) DEFAULT NULL
-  `item_price` DOUBLE(10) DEFAULT NULL
-  PRIMARY KEY ( `Item_id` INT (5))
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+  `id` INT(5)  AUTO_INCREMENT,
+  `item_name` VARCHAR(40) ,
+  `cost` DOUBLE(10,2) ,
+  PRIMARY KEY ( `id`)
 );
-CREATE TABLE IF NOT EXISTS `ims` . `orders` (
-  `Order_id INT(5) NOT NULL AUTO_INCREMENT,
-  `Order_name` VARCHAR(40) DEFAULT NULL
-  `Order_total DOUBLE(10) DEFAULT NULL
-  PRIMARY KEY ( `Order_id` INT (5))
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+  `Order_id` INT(5)  AUTO_INCREMENT,
+  `Order_name` VARCHAR(40) ,
+  PRIMARY KEY ( `Order_id` )
 );
 
 
